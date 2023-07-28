bd5682ff6967019bc8f60e382350801d80b42975
Nicole Silvestrini <nicole.silvestrini@ws-solution.com>
2023-07-28 16:02:16 -0300

    Correções da herança

--- netflix.dart
+++ netflix.dart
@@ -9,14 +9,17 @@ void main() {
   var senha_alterada = "senha_alterada";
   var alteracao = cadastro.alterar_senha(senha_alterada);
   print("Nova senha: " + alteracao);
+  print(cadastro.senha);
   print("\nClasse Continuar_assistindo");
   var filme_parametro = "The Flash";
   var continuar_assistindo =
-      Continuar_assistindo(email_parametro, filme_parametro);
+      Continuar_assistindo(email_parametro, senha_parametro, filme_parametro);
+  print("Email: " + continuar_assistindo.email);
   print("Filme: " + continuar_assistindo.filme);
 }
 
 class Cadastro {
+  //Atributo
   final String nome = "Nicole";
   late String email;
   late String senha;
@@ -25,14 +28,16 @@ class Cadastro {
     senha = senha_parametro;
   }
   String alterar_senha(String senha_alterada) {
+    senha = senha_alterada;
     return senha_alterada;
   }
 }
 
 class Continuar_assistindo extends Cadastro {
   late String filme;
-  Continuar_assistindo(String email_parametro, String filme_parametro) {
-    email = email_parametro;
+  Continuar_assistindo(
+      String email_parametro, String senha_parametro, String filme_parametro)
+      : super(email_parametro, senha_parametro) {
     filme = filme_parametro;
   }
 }
